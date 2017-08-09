65b38f3ab5f31bb093c88f33289508a8704a0cc4
Daniel Corzo <dxcorzo@gmail.com>
2017-08-08 22:51:31 -0500

    Initial commit

---
+++ ArduinoDataReader/src/Main.java
@@ -0,0 +1,31 @@
+import javax.swing.*; 
+
+public class Main 
+{
+	private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Sensor de Humedad");
+        frame.setBounds(100, 100, 635, 365);
+        
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+	
+	public static void main(String[] args) 
+	{
+		javax.swing.SwingUtilities.invokeLater(new Runnable() 
+		{
+            public void run() 
+            {
+                createAndShowGUI();
+            }
+        });
+	}
+}
